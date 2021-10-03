8624372241285e8ec16b227ea5ef82dc33398eee
Niru0290 <90973750+Niru0290@users.noreply.github.com>
2021-10-03 16:22:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Niranjan!"); 
-		 System.out.println("Hello, Test!");
+		 System.out.println("1212, Test!");
     }
 }
